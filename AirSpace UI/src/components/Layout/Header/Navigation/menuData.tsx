58ad972d9d6930fef3bc0e27730427bcda0c8291
Atharva Lade <ladeatharva@gmail.com>
2025-02-25 14:45:40 -0600

    final UI

--- AirSpace UI/src/components/Layout/Header/Navigation/menuData.tsx
+++ AirSpace UI/src/components/Layout/Header/Navigation/menuData.tsx
@@ -4,4 +4,5 @@ export const headerData: HeaderItem[] = [
   { label: "Buy & Sell", href: "/#main-banner" },
   { label: "Perks", href: "/#portfolio" },
   { label: "Listings", href: "/listings" },
+  { label: "Web3 Dashboard", href: "/dashboard" }
 ];
